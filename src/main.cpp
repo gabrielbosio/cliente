854245e4227dcfd0c70dba7d589ed944a8fd382d
gabrielbosio <gabrielbosio95@gmail.com>
2019-11-21 02:20:22 -0300

    Remueve estructura Parametros sin usar

--- src/main.cpp
+++ src/main.cpp
@@ -27,14 +27,6 @@ Display* display;
 TecladoCliente* tecladoCliente;
 MatrizLeds* matrizLeds;
 Tester* tester;
-
-// Parametros de la tarea a ejecutar en paralelo
-typedef struct Parametros {
-    Display* display;
-    int numero;
-} Parametros;
-
-Parametros parametros;
 TaskHandle_t tareaDisplay;
 
 // Tarea a ejecutar en paralelo
@@ -48,9 +40,6 @@ void setup() {
     Serial.begin(115200);
     display = new Display(SEGMENTO_A, SEGMENTO_B, SEGMENTO_C, SEGMENTO_D, SEGMENTO_E, SEGMENTO_F,
                           SEGMENTO_G, MUX_SEGMENTO_0, MUX_SEGMENTO_1);
-    
-    parametros.display = display;
-    parametros.numero = 42;
 
     xTaskCreatePinnedToCore(loopSegundoCore, "loopSegundoCore", 4096, NULL, 1, &tareaDisplay, 1);
     tecladoCliente = new TecladoCliente(PIN_SWITCH_1, PIN_SWITCH_2, PIN_SWITCH_3, PIN_SWITCH_4,
